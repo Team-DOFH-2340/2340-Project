2c957d7988465c302d35386c808957a0efd2edf6
William Broniec <williambroniec@gmail.com>
2016-09-02 14:02:48 -0400

    FXML for login screen
    Basic layout of login screen done - will need more work later.

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.fxml.*;
+
+public class Test extends Application {
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+
+		Scene scene = new Scene(root, 800, 600);
+		stage.setTitle("Welcome");
+		stage.setScene(scene);
+		stage.show();
+	}
+}
