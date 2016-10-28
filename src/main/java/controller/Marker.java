bf23aade544fb86ca6c9b0b47c6259a475859461
Hayden Flinner <hayden@gatech.edu>
2016-10-27 22:06:31 -0400

    Added marker extension.

---
+++ src/main/java/controller/Marker.java
@@ -0,0 +1,39 @@
+package controller;
+
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.InfoWindow;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
+import model.Report;
+
+/**
+ * Created by hayde on 10/27/2016.
+ */
+public class Marker extends com.lynden.gmapsfx.javascript.object.Marker {
+    private InfoWindow window;
+    private boolean windowVisible;
+    private GoogleMap map;
+    private Report report;
+
+    public Marker(MarkerOptions mo, GoogleMap map, Report report) {
+        super(mo);
+        windowVisible = false;
+        this.report = report;
+    }
+
+    public InfoWindow getWindow() {
+        return window;
+    }
+
+    public void setWindow(InfoWindow window) {
+        this.window = window;
+    }
+
+    public void toggleWindowVisibility() {
+        if (windowVisible) {
+            window.close();
+        } else {
+            window.open(map, this);
+        }
+        this.windowVisible = !this.windowVisible;
+    }
+}
